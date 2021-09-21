c914afd794bc685fa4174d3ba9d93b8a588b37b1
Rastar Music <rastarmusic@MacBook-Pro.local>
2021-09-21 00:35:51 -0400

    start timer

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-var startGame = document.querySelector("button");
+
+var startQuiz = document.querySelector("#buttonstart");
+var countEl = document.querySelector("#count");
+
+var count = 76;
+
+
+function setCounterText() {
+  countEl.textContent = count;
+}
+
+startQuiz.addEventListener("click", function() {
+ 
+  if (count > 0) {
+    count--;
+    setCounterText();
+
+  }
+});
+
+
