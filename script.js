0094479e7a9eb2652d475976ef82851af3d9348e
Rastar Music <rastarmusic@MacBook-Pro.local>
2021-09-21 22:41:53 -0400

    Timer and First Question

--- script.js
+++ script.js
@@ -1,21 +1,38 @@
 
-var startQuiz = document.querySelector("#buttonstart");
-var countEl = document.querySelector("#count");
+var startQuiz = document.querySelector("#buttonstart").addEventListener("click", startQuiz);
+var timerEl = document.querySelector("#count");
+var questionEl = document.getElementById("questions");
+var timeLeft = 75;
 
-var count = 76;
 
-
-//function setCounterText() {
-  //countEl.textContent = count;
-//}
-
-startQuiz.addEventListener("click", function() {
- 
   
-    //setCounterText();
-    var myCounter=setInterval(function{
-        count--
-        },1000)
-        )
+function startQuiz(){
+
+//This is my timer
+  var timeInterval = setInterval(function () {
     
-    
+    if (timeLeft > 1) {
+      timerEl.textContent = timeLeft;
+      timeLeft--;
+    } else if (timeLeft === 1) {
+      timerEl.textContent = timeLeft;
+      timeLeft--;
+    } else {
+      
+      timerEl.textContent = timeLeft;
+      
+      clearInterval(timeInterval);
+     
+      displayMessage("Time is Up!");
+    }
+  }, 1000);
+
+//replacing the original html content with the first question
+var question1 = "Which of these is a javascript file??";
+  questionEl.textContent = question1; 
+
+  
+
+//var question2 = "This is Question 2 ?";
+//questionEl.textContent = question2;
+}     
